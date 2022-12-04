f76b0b37fd53ce1a255bf8209ffd02689844f33d
dmuscinschi <dmuscinschi@griddynamics.com>
2022-12-04 20:51:33 +0200

    feat: add buttons hover effect and add burger menu

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
 import './sass/style.scss';
+
+const burgerBtn = document.querySelector('.org-burger-button');
+const menu = document.querySelector('.org-burger');
+const menuLinks = document.querySelectorAll('.org-burger__item');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('burger--active');
+  menu.classList.toggle('burger--active');
+  if (menu.classList.contains('burger--active')) {
+    disableScrolling();
+  } else enableScrolling();
+});
+
+const closeMenu = () => {
+  burgerBtn.classList.remove('burger--active');
+  menu.classList.remove('burger--active');
+  enableScrolling();
+};
+
+menuLinks.forEach((link) => link.addEventListener('click', closeMenu));
+
+document.onclick = (event) => {
+  if (event.target != menu && event.target.parentNode != burgerBtn) {
+    closeMenu();
+  }
+};
+
+const disableScrolling = () => {
+  var x = window.scrollX;
+  var y = window.scrollY;
+  window.onscroll = function () {
+    window.scrollTo(x, y);
+  };
+};
+
+const enableScrolling = () => {
+  window.onscroll = function () {};
+};
