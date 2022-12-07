9deb768f10bb74e992cf5e205544e418c08f4eba
dmuscinschi <dmuscinschi@griddynamics.com>
2022-12-07 16:09:39 +0200

    refactor: change flex on grid for products and products-showcase, add new variables, change box-shadow on backdrop and other changes

--- main.js
+++ main.js
@@ -3,25 +3,37 @@ import './sass/style.scss';
 const burgerBtn = document.querySelector('.org-burger-button');
 const menu = document.querySelector('.org-burger');
 const menuLinks = document.querySelectorAll('.org-burger__item');
+const backdrop = document.createElement('div');
+document.body.append(backdrop);
 
-burgerBtn.addEventListener('click', () => {
+burgerBtn.addEventListener('click', (event) => {
+  console.log(event.target);
   burgerBtn.classList.toggle('burger--active');
   menu.classList.toggle('burger--active');
+  backdrop.classList.toggle('backdrop');
   if (menu.classList.contains('burger--active')) {
     disableScrolling();
   } else enableScrolling();
+
+  // create backdrop element
 });
 
 const closeMenu = () => {
   burgerBtn.classList.remove('burger--active');
   menu.classList.remove('burger--active');
+  backdrop.classList.remove('backdrop');
+
   enableScrolling();
 };
 
 menuLinks.forEach((link) => link.addEventListener('click', closeMenu));
 
 document.onclick = (event) => {
-  if (event.target != menu && event.target.parentNode != burgerBtn) {
+  if (
+    event.target != menu &&
+    event.target != burgerBtn &&
+    event.target.tagName.toLowerCase() !== 'span'
+  ) {
     closeMenu();
   }
 };
